e2f2eae94068d090039b88d8e458a5857c7d27e7
Joosang Kang <tsusaikang@gmail.com>
2020-02-05 01:47:38 +0900

    minor fix

--- app/src/main/java/com/kangjusang/musicismylife/MainActivity.java
+++ app/src/main/java/com/kangjusang/musicismylife/MainActivity.java
@@ -17,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enableNavigationUI();
+
+        getApplication().setTheme(R.style.Theme_AppCompat);
+
+    }
+
+    private void enableNavigationUI() {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
